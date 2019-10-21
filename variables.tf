97cbbcfda0093cc60355cf22d9156afa5c1abaac
shathedawn <37858927+shathedawn@users.noreply.github.com>
2019-10-21 11:59:06 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -4,7 +4,7 @@ variable "counts" {
 
 variable "key_name" {
   description = "Private key name to use with instance"
-  default     = "terraform"
+  default     = "sharique"
 }
 
 variable "instance_type" {
